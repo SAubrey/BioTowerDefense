b108109a7b9a4443792f2c252018296340078e5e
slifcoa <slifcoa@mail.gvsu.edu>
2018-12-05 18:31:53 -0500

    Minor UI tweaks, dynamically loads HP icon based on level

---
+++ Assets/Scripts/hpIcon.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class hpIcon : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GameObject.Find("HP Image").GetComponent<Image>().sprite = gameObject.GetComponent<SpriteRenderer>().sprite;
+		Destroy(this);
+	}
+}
